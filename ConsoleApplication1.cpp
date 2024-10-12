d4a7f28a93b5c282b23962dad628dd99da8ab57b
PiotruxonProgramuje <piotruxonnik@gmail.com>
2024-10-12 20:07:00 +0200

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,30 +4,30 @@ int main()
 {
     std::string name;
     int age;
-    std::cout << "Ile masz lat? : ";
+    std::cout << "How old are you? : ";
     std::cin >> age;
 
     if (age <= 13) {
-        std::cout << "Twoja grupa wiekowa to 1-13\n";
+        std::cout << "Your age group is 1-13\n";
     }
     else if (age >= 14 && age < 18) {
-        std::cout << "Twoja grupa wiekowa to 14-17\n";
+        std::cout << "Your age group is 14-17\n";
     }
     else if (age >= 18 && age < 30) {
-        std::cout << "Twoja grupa wiekowa to 18-30\n";
+        std::cout << "Your age group is 18-30\n";
     }
     else if (age >= 30) {
-        std::cout << "Twoja grupa wiekowa to 30+\n";
+        std::cout << "Your age group is 30+\n";
     }
 
-    std::cout << "Jak masz na imie? :";
+    std::cout << "Whats your name? :";
     std::cin >> name;
 
-    std::cout << "Masz " << age << " lat\n";
-    std::cout << "Twoje imie to " << name << "\n";
-    std::cout << "Program stworzony przez Piotruxona v1.665\n";
+    std::cout << "You are " << age << " years old\n";
+    std::cout << "Your name is " << name << "\n";
+    std::cout << "App created by Piotruxon (v1.665)\n";
 
-    std::cout << "Nacisnij Enter, aby zamknac program...\n";
+    std::cout << "Press ENTER to close the app...\n";
     std::cin.ignore(); // Ignoruje pozostałe dane w buforze
     std::cin.get();    // Czeka na wciśnięcie Enter
 
